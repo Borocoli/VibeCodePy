b9e97691d6feab229341abffc0c30b355cdba3d6
Broccoli <42372902+Borocoli@users.noreply.github.com>
2025-10-30 16:01:35 +0200

    Update f.py

--- f.py
+++ f.py
@@ -1,5 +1,5 @@
 from .main import getLine, chat, context, PROMPTER
-
+from .abscontext import ContextObj
 
 def __getattr__(name):
     global context
@@ -8,10 +8,16 @@ def __getattr__(name):
     def _catchParams(*args, **kwargs):
         pargs = [type(a).__name__ for a in args]
         pkargs = [str(k) + ' of type ' + type(v).__name__ for k, v in kwargs.items()]
-        prompt = PROMPTER.prompt(name, 'f', comment, pargs, pkargs)
+        descr = None
+        if '_description' in kwargs:
+            descr = kwargs['_description']
+            del kwargs['_description']
+        prompt = PROMPTER.prompt(name, 'f', comment, pargs, pkargs, description=descr)
         response = chat(prompt)
-        context[name] = response
         print(response)
         exec(response, globals = globals())
+        
+        tmp_context = ContextObj(name, response, descr)
+        context.add_definition(tmp_context)
         return globals()[name](*args, **kwargs)
     return _catchParams
